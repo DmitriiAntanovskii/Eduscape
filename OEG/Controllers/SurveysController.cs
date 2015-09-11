167f2739c116a3ff6b53c7c93c1a6b1c7c1e74ea
thinksimpleit <tony@thinksimple.com.au>
2015-09-11 10:53:18 +1000

    added surveys nscaffolding

---
+++ OEG/Controllers/SurveysController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OEG.Models;
+
+namespace OEG.Controllers
+{
+    public class SurveysController : Controller
+    {
+        private oeg_reportsEntities db = new oeg_reportsEntities();
+
+        // GET: Surveys
+        public ActionResult Index()
+        {
+            return View(db.Surveys.ToList());
+        }
+
+        // GET: Surveys/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Surveys surveys = db.Surveys.Find(id);
+            if (surveys == null)
+            {
+                return HttpNotFound();
+            }
+            return View(surveys);
+        }
+
+        // GET: Surveys/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Surveys/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SurveyID,SurveyName,SurveyCode,CreatedBy,CreatedDate,ModifedBy,ModifiedDate")] Surveys surveys)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Surveys.Add(surveys);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(surveys);
+        }
+
+        // GET: Surveys/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Surveys surveys = db.Surveys.Find(id);
+            if (surveys == null)
+            {
+                return HttpNotFound();
+            }
+            return View(surveys);
+        }
+
+        // POST: Surveys/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SurveyID,SurveyName,SurveyCode,CreatedBy,CreatedDate,ModifedBy,ModifiedDate")] Surveys surveys)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(surveys).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(surveys);
+        }
+
+        // GET: Surveys/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Surveys surveys = db.Surveys.Find(id);
+            if (surveys == null)
+            {
+                return HttpNotFound();
+            }
+            return View(surveys);
+        }
+
+        // POST: Surveys/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Surveys surveys = db.Surveys.Find(id);
+            db.Surveys.Remove(surveys);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
